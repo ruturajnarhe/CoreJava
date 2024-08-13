7af601c14c4a6accc3caa9a2c221c8c2f9fd084b
ruturajnarhe <ruturajnarhe@gmail.com>
2024-08-13 12:37:43 +0530

    new commit

--- String/String/FirstOccurance.java
+++ String/String/FirstOccurance.java
@@ -1,17 +1,17 @@
 package String;
 
-public class FindOccurance {
+public class FirstOccurance {
     public static void main(String[] args) {
 
         String str = "Ruturaj";
 
         // Method...
-        System.out.println("By Method : " + str.indexOf('r'));
+        System.out.println("By Method : " + str.indexOf('a'));
 
         char ch[] = str.toCharArray();
 
         // Logic...
-        char s = 'r';
+        char s = 'a';
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == s) {
                 System.out.println("By Logic : " + i);
